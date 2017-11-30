7e21b6a621f922851d55f10e3e561fc3e22389a6
恋在那时 <dzfqhx@163.com>
2017-11-30 21:54:10 +0800

    添加了对redis的支持

---
+++ ssm-service/src/main/java/com/dzf/redis/RedisClient.java
@@ -0,0 +1,64 @@
+package com.dzf.redis;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * redis 客户端
+ * 使用redis的主从复制
+ * @author dingzf
+ * @date 2017年11月30日
+ * @time 21:23:56
+ */
+
+@Component
+public class RedisClient {
+
+	@Resource 
+	private Jedis masterJedis;
+	
+	@Resource
+	private Jedis slaveJedis;
+	
+	public void setKey(String name,String value){
+		masterJedis.set(name, value);
+	}
+	public String getKey(String name){
+		return slaveJedis.get(name);
+	}
+	/**
+	 * 设置过期时间，可以用来将短信验证码存在这里
+	 * @param name
+	 * @param expire
+	 */
+	public Long setKeyExpired(String name,int expire){
+		return masterJedis.expire(name, expire);
+	}
+	/**
+	 * 操作list
+	 */
+	public Long setKeys(String key,String ... value){
+		return masterJedis.lpush(key, value);
+	}
+	
+	public List<String> getKeys(String key){
+		return slaveJedis.lrange(key, 0, -1);//表示获取所有的值
+	}
+	
+	/**
+	 * 操作hash
+	 */
+	public String setHash(String key,Map<String,String> map){
+		return masterJedis.hmset(key, map);
+	}
+	public List<String> getHash(String key,String ...fields){
+		return slaveJedis.hmget(key, fields);
+	}
+	
+}
---
+++ ssm-service/src/main/java/com/dzf/redis/RedisConfig.java
@@ -0,0 +1,66 @@
+package com.dzf.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * reids配置类
+ * 
+ * @author dingzf
+ * @date 2017年11月30日
+ * @time 20:59:47
+ */
+@Configuration
+public class RedisConfig {
+	@Value("${redis.maxIdle}")
+	private Integer maxIdle;
+	@Value("${redis.maxTotal}")
+	private Integer maxTotal;
+	@Value("${redis.maxWaitMills}")
+	private Integer maxWaitMills;
+	@Value("${redis.timeout}")
+	private Integer timeout;
+
+	//master
+	@Value("${redis.masterHost}")
+	private String masterHost;
+	@Value("${redis.masterPort}")
+	private Integer masterPort;
+	@Value("${redis.masterPassword}")
+	private String masterPassword;
+	//slave
+	@Value("${redis.slaveHost}")
+	private String slaveHost;
+	@Value("${redis.slavePort}")
+	private Integer slavePort;
+	@Value("${redis.slavePassword}")
+	private String slavePassword;
+	
+	@Bean(name="masterJedis")
+	private Jedis getMasterRedis(){
+		return intiJedis(masterHost,masterPassword,masterPort,timeout);
+	}
+	@Bean(name="slaveJedis")
+	private Jedis getSlaveRedis(){
+		return intiJedis(slaveHost,slavePassword,slavePort,timeout);
+	}
+	private Jedis intiJedis(String host, String password, Integer port,Integer timeout) {
+		JedisPoolConfig pool = new JedisPoolConfig();
+		pool.setMaxIdle(maxIdle);
+		pool.setMaxTotal(maxTotal);
+		pool.setMaxWaitMillis(maxWaitMills);
+		JedisPool jedis = new JedisPool(pool, host, port, timeout, password);
+		return jedis.getResource();
+	}
+	public static void closeJedis(Jedis jedis){
+		if(jedis!=null){
+			jedis.close();
+		}
+	}
+	
+}
