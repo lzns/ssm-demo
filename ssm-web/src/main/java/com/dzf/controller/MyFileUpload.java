42ccea31d401d5224663e78f5e6e5dbd6ad47dd0
恋在那时 <dzfqhx@163.com>
2018-01-21 23:14:10 +0800

    本次更新： 1.单个文件上传使用普通表单提交 2.单个文件下载 3.一次下载多个文件，把多个文件放到一个压缩包里

--- ssm-web/src/main/java/com/dzf/controller/MyFileUpload.java
+++ ssm-web/src/main/java/com/dzf/controller/MyFileUpload.java
@@ -1,14 +1,19 @@
 package com.dzf.controller;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUpload;
@@ -21,7 +26,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 /**
  * 文件上传测试
  * @author dingzf 
@@ -30,12 +37,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping("load")
-@MultipartConfig
 public class MyFileUpload {
 
 	private static Logger log = LoggerFactory.getLogger(MyFileUpload.class);
 	/**
-	 * common FileUpload jar 包里提供的 适用servlet 3.0 以下的文件上传
 	 * 
 	 * @param request
 	 * @return
@@ -44,9 +49,6 @@ public class MyFileUpload {
 	@ResponseBody
 	private String upload1(HttpServletRequest request,HttpServletResponse response) {
 		try {
-			request.setCharacterEncoding("utf-8");
-			response.setCharacterEncoding("utf-8");
-			response.setContentType("aplication/json;charset=utf-8");
 			RequestContext context = new ServletRequestContext(request);
 			if (FileUpload.isMultipartContent(context)) { //判断是否有
 				//创建磁盘文件工厂 
@@ -70,7 +72,6 @@ public class MyFileUpload {
 						File file = new File(path+"/"+name);
 						InputStream in = fileItem.getInputStream();
 						FileUtils.copyInputStreamToFile(in, file);
-//						fileItem.write(file); 
 					}
 				}
 				return "文件上传成功";
@@ -81,62 +82,94 @@ public class MyFileUpload {
 			e.printStackTrace();
 			return "文件上传失败";
 		}
-
 	}
 
 	/**
-	 * 使用servlet 3.0 以上版本的文件上传
 	 * 
 	 * @param request
 	 * @return
 	 */
 	@RequestMapping("/upload2")
 	@ResponseBody
-	private String upload2(HttpServletRequest request,HttpServletResponse response) {
+	private String upload2(@RequestParam("file")CommonsMultipartFile partFile,HttpServletRequest request) {
 		try {
 			String path = request.getServletContext().getRealPath("/upload");
+			String name = request.getParameter("name");
+			log.info("其他的参数{}",name);
 			log.info("upload2---------------start---------------------");
-			Collection<Part> parts = request.getParts();
-			log.info("upload2-parts------"+parts);
-			//log.info(String format,object)的使用方法
 			log.info("这个临时文件的路径是[{}]", path);
-			for (Part part : parts) {
-				String header = part.getHeader("Content-Disposition");
-				System.out.println(header);
-				//需要对header进行处理  最后一个是 filename=文件名
-				int index = header.lastIndexOf("="); //获取最后一个=的索引
-				String fileName = header.substring(index+1);
-				System.out.println(fileName);
+			String filename = partFile.getOriginalFilename();
+			log.info("文件的名字：{}",filename);
+			File file = new File(path+"/"+filename);
+			InputStream inputStream = partFile.getInputStream();
+			FileUtils.copyInputStreamToFile(inputStream, file);
+			if(inputStream!=null){
+				inputStream.close();
 			}
-			
-//			Part part = request.getPart("file2");
-//			String header = part.getHeader("Content-Disposition");
-//			System.out.println(header);
-//			//需要对header进行处理  最后一个是 filename=文件名
-//			int index = header.lastIndexOf("="); //获取最后一个=的索引
-//			String fileName = header.substring(index+1);
-//			System.out.println(fileName);
-			// 
-//			part.write(fileName);
-			
+			return "文件上传成功！";
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "文件上传失败！";
 		} 
-		return null;
 	}
 
 	/**
-	 * MultipartRequest 方式实现
-	 * 
+	 * 文件下载
+	 * 单个文件下载
 	 * @param request
 	 * @return
+	 * @throws IOException 
 	 */
-	@RequestMapping("/upload3")
-	@ResponseBody
-	private String upload3(HttpServletRequest request,HttpServletResponse response) {
-
-		return null;
+	@RequestMapping("/down1")
+	private void down(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		String path = request.getServletContext().getRealPath("/upload");
+		File file = new File(path);
+		File[] files = file.listFiles();
+		String name = files[0].getName();
+		System.out.println("文件的名字："+name);
+		response.addHeader("content-disposition", "attachment;filename="+name);
+		FileUtils.copyFile(files[0], response.getOutputStream());
 	}
-
+	/**
+	 * 文件下载，一下次下载多个文件
+	 * 思路：先将多个文件压缩到一个压缩包里去，然后传到前台
+	 * @param request
+	 * @return
+	 * @throws IOException 
+	 */
+	@RequestMapping("/down2")
+	private void down2(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		String path = request.getServletContext().getRealPath("/upload");
+		File file = new File(path);
+		File[] files = file.listFiles();
+		File zipFile =new File("test.zip");
+		if(!zipFile.exists()){
+			zipFile.createNewFile();
+		}
+		String zipName = zipFile.getName();
+		log.info("压缩文件的名字：{}",zipName);
+		response.addHeader("Content-Disposition", "attachment;filename="+zipName);
+		//定义输出类型
+//		response.setContentType("application/zip");
+		ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(zipFile));
+		BufferedInputStream in  =null;
+		log.info("文件的个数{}",files.length);
+		for(int i = 0;i<files.length;i++){
+			String name = files[i].getName();
+			System.out.println("文件的名字："+name);
+			ZipEntry zipEntry = new ZipEntry(name);
+			zip.putNextEntry(zipEntry);
+			in = new BufferedInputStream(new FileInputStream(files[i]));
+			int len = 0;
+			byte [] btyes = new byte[1024*4];
+			while((len=in.read(btyes))!=-1){
+				zip.write(btyes, 0, len);
+			}
+		}
+		zip.flush();
+		zip.close();
+		in.close();
+		FileUtils.copyFile(zipFile, response.getOutputStream());
+	}
+	
 }
--- ssm-web/target/ssm-web-0.0.1-SNAPSHOT/fileupload.jsp
+++ ssm-web/target/ssm-web-0.0.1-SNAPSHOT/fileupload.jsp
@@ -5,49 +5,57 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>文件上传第一种 servlet3.0 以下</title>
-<script src="static/js/jquery.min.js"></script>
+<script  type="text/javascript" src="static/js/jquery.min.js"></script>
 </head>
 <script type="text/javascript">
-function load(){
-	alert("试试。。。");
-}
-	//文件上传使用ajax
-	function fileupload(){
+function fileupload1(){
+	$("#form1").ajaxSubmit();
+
+		//var form = new FormData($("#form1"));
+		/* var form = new FormData(document.getElementById("form1"));
 		$.ajax({
 			url:"load/upload1",
 			type:"post",
-			data:$("#form1").serialize(),
-			dataype:'json',
+			data:form,
 			success:function(data){
 				alert(data);
 			},
 			error:function(data){
-				return data;
+				alert("后台发生异常");
 			},
 			cache:false,
 			async:false
-		});
-	
-	}
+		}); */
+}
+function fileupload2(){
 
+
+}
 </script>
-<body onload="load()">
+<body >
 <h1>第一种 ，fileUpload的实现方式</h1>
 <form  id ="form1" action="load/upload1" enctype="multipart/form-data" method="post">
-	<input type = "file" name= 'file1' />
+	<input type = "file" name= 'file' />
 	<input type="text" name="name" value="dzf"/>
-	<input type="button" id = "button1" value="ajax上传" onclick="fileupload()">
+	<input type="button" id = "button1" value="ajax上传" onclick="fileupload1()">
 	<input type ="submit" value="直接上传">
 </form> 
 <hr>
 
 <h1>第二种，part的实现方式</h1>
 <form  id ="form2" action="load/upload2" enctype="multipart/form-data" method="post">
-	<input type = "file" name= 'file2' />
+	<input type = "file" name= 'file' />
 	<input type="text" name="name" value="dzf"/>
 	<input type="button" id = "button2" value="ajax上传" onclick="fileupload2()">
 	<input type ="submit" value="直接上传">
 </form> 
-
+<h1>普通文件下载</h1>
+<form action="load/down1" name="form3" id = "form3" method="post">
+	<input type = "submit" value="普通文件下载">
+</form>
+<h1>压缩文件下载</h1>
+<form action="load/down2" name="form3" id = "form3" method="post">
+	<input type = "submit" value="压缩文件下载">
+</form>
 </body>
 </html>
